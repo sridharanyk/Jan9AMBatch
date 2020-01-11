d75f21191afa425bdbe624aecfdb453cd1792183
sridharan <sridharanyk@gmail.com>
2020-01-11 11:19:17 +0530

    Flower code

---
+++ src/org/lily/Bussiness.java
@@ -0,0 +1,14 @@
+package org.lily;
+
+public class Bussiness 
+{
+public void Tulips() 
+{
+System.out.println("their is 30% off on tulips");
+}
+public void Jasmin() 
+{
+System.out.println("their is 50% off on jasmin");
+}
+
+}
---
+++ src/org/lily/party.java
@@ -0,0 +1,32 @@
+package org.lily;
+
+public class party extends Bussiness
+{
+	@Override
+	public void Tulips() 
+	{
+		System.out.println("theis is 60% off on Tulips");
+		
+	}
+	@Override
+	public void Jasmin()
+	{
+	System.out.println("their is 70% off on Jasmin");	
+	}
+	public static void main(String[] args) 
+	{
+	party p=new party();
+	p.Tulips();
+	p.Jasmin();
+	}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
